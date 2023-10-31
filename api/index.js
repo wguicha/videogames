fe96764be7baaee698eefe8ae8624b41dde6f689
WilliamG <williamalberto1@hotmail.com>
2023-10-30 20:53:59 -0500

    Prueba 5 deploy

--- api/index.js
+++ api/index.js
@@ -21,8 +21,8 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(3001, () => {
-    console.log('Is listening at 3001'); // eslint-disable-line no-console
+    console.log('Is listening'); // eslint-disable-line no-console
   });
 });
